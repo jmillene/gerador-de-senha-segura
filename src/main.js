a251edfd39eacb6a0fd5e01761f77dd793d3a5fc
jessica <jmillene12@gmail.com>
2022-11-23 16:08:05 -0300

    add clipboard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { nanoid } from 'nanoid'
+import  copy  from 'clipboard-copy'
+
 import "./style.css";
 const passwordBtn = document.querySelector('button');
 const displayPassword = document.querySelector('h2');
 
 passwordBtn.addEventListener('click', () =>{
   displayPassword.innerHTML = nanoid();
+})
+displayPassword.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
 })
