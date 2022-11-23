c1f70c3852825ebdcd0a59ca8d7842e68df3fe3c
jessica <jmillene12@gmail.com>
2022-11-23 15:42:40 -0300

    gerador de senha aleatorias

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello Word');
+import { nanoid } from 'nanoid'
+import "./style.css";
+const passwordBtn = document.querySelector('button');
+const displayPassword = document.querySelector('h2');
+
+passwordBtn.addEventListener('click', () =>{
+  displayPassword.innerHTML = nanoid();
+})
